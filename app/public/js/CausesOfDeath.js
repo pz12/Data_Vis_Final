88c991b813d9e92c967348bacd14e10fd906e6c3
jflouis <jeanfredo.louis@utah.edu>
2017-11-12 16:05:20 -0700

    Adding Cause of Death data

---
+++ app/public/js/CausesOfDeath.js
@@ -0,0 +1,18 @@
+/**
+* Display the causes of death using the ICD Codes and the display text of those ICDs
+*
+*/
+
+
+class CausesOfDeath {
+
+    //Initializes the svg elements required for this chart;
+
+ constructor(){
+     this.divCausesChart = d3.select("#a-container");
+ };
+
+
+
+
+}
